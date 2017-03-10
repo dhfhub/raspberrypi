2040ff3d974644880c30c8c6b82b7f835c7ba6a5
kurima <srt.urthr7@gmail.com>
2017-03-10 23:37:37 +0900

    servo test

---
+++ motor/servotest.py
@@ -0,0 +1,31 @@
+﻿import wiringpi
+
+#servo は VS-S092J
+
+PWM_PIN = 18   # GPIOの18番ピン
+DUTY_MAX = 115 # 90°の時
+DUTY_MIN = 38  # -90°の時
+DUTY_HOME= 77  # 0°の時
+duty = 0       # Duty比の値(0から1024)
+
+wiringpi.wiringPiSetupGpio()   #winringPiの初期設定
+wiringpi.pinMode(PWM_PIN, wiringpi.GPIO.PWM_OUTPUT)
+#「Balanced」→「mark:space」Modeに変更
+wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+# 周波数を50Hzにすると、18750/周波数=375
+wiringpi.pwmSetClock(375)
+
+wiringpi.pwmWrite(PWM_PIN, DUTY_HOME)        # 0°の位置に移動
+wiringpi.delay(100) #0.1sec
+
+for duty in range(DUTY_HOME, DUTY_MAX+1, 1):  # 0°から90°まで動かす
+	wiringpi.pwmWrite(PWM_PIN, duty)
+	wiringpi.delay(100) #0.1sec
+wiringpi.delay(500)    #0.5sec
+for duty in range(DUTY_MAX, DUTY_MIN-1, -1):  # 90°から-90°まで動かす
+	wiringpi.pwmWrite(PWM_PIN, duty)
+	wiringpi.delay(100)
+wiringpi.delay(500)
+
+wiringpi.pwmWrite(PWM_PIN, DUTY_HOME)        # 0°の位置に移動
+
